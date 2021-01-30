e222c1436405294fb956bdbf10ace6d25ffac29f
George <71333847+georgeclm@users.noreply.github.com>
2021-01-30 19:25:46 +0700

    Update BillDivision.py

--- BillDivision.py
+++ BillDivision.py
@@ -1,6 +1,3 @@
 def bonAppetit(bill, k, b):
     bill[k] = 0
-    if sum(bill) / 2 == b:
-        print("Bon Appetit")
-    else:
-        print(abs(sum(bill) // 2 - b))
+    print("Bon Appetit") if sum(bill) /2 ==b else print(int(abs(sum(bill) / 2 - b )))
